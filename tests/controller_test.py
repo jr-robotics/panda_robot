b41bff483ec1aea9da7113557136d3ef780a239c
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2021-02-10 11:53:20 +0100

    Further changes to improve readability

--- tests/controller_test.py
+++ tests/controller_test.py
@@ -7,27 +7,36 @@ from copy import deepcopy
 
 from rospy.timer import sleep
 
-names = ['panda_joint1', 'panda_joint2', 'panda_joint3',
-         'panda_joint4', 'panda_joint5', 'panda_joint6', 'panda_joint7']
+joint_names = ['panda_joint1', 'panda_joint2', 'panda_joint3',
+               'panda_joint4', 'panda_joint5', 'panda_joint6', 'panda_joint7']
+
+
+def _print_values(joint_values, delta, count):
+    if count % 500 == 0:
+        print('values: {} -- delta {}'.format(joint_values, delta))
+        print ('\n ----  \n')
+        print (' ')
+    count += 1
+
 
 if __name__ == '__main__':
 
     rospy.init_node('test_node')
-    r = PandaArm()
+    panda_arm = PandaArm()
 
     rate = rospy.Rate(400)
 
     elapsed_time_ = rospy.Duration(0.0)
     period = rospy.Duration(0.005)
 
-    r.untuck()  # move to neutral pose before beginning
+    panda_arm.untuck()  # move to neutral pose before beginning
 
-    initial_pose = deepcopy(r.joint_ordered_angles())
+    initial_pose = deepcopy(panda_arm.joint_ordered_angles())
 
     # raw_input('Hit Enter to Start')
     sleep(5)
     print('commanding')
-    vals = deepcopy(initial_pose)
+    joint_values = deepcopy(initial_pose)
     count = 0
 
     while not rospy.is_shutdown():
@@ -37,20 +46,16 @@ if __name__ == '__main__':
         delta = 3.14 / 16.0 * \
             (1 - np.cos(3.14 / 5.0 * elapsed_time_.to_sec())) * 0.2
 
-        for j, v in enumerate(vals):
-            if j == 4:
-                vals[j] = initial_pose[j] - delta
+        for joint, _ in enumerate(joint_values):
+            if joint == 4:
+                joint_values[joint] = initial_pose[joint] - delta
             else:
-                vals[j] = initial_pose[j] + delta
+                joint_values[joint] = initial_pose[joint] + delta
 
-        if count % 500 == 0:
-            print('values: {} -- delta {}'.format(vals, delta))
-            print ('\n ----  \n')
-            print (' ')
+        _print_values(joint_values, delta, count)
 
-        r.set_joint_positions_velocities(
-            vals, [0.0 for _ in range(7)])  # for impedance control
+        panda_arm.set_joint_positions_velocities(
+            joint_values, [0.0 for _ in range(7)])  # for impedance control
         # r.set_joint_positions(vals) # try this for position control
 
-        count += 1
         rate.sleep()
